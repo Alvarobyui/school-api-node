7364b6f313e390d11acb0cc9bb70ab0cc01f9b0e
Alvarobyui <u701860138@byui.edu>
2024-02-12 22:06:13 -0500

    add

--- controllers/student.js
+++ controllers/student.js
@@ -1,14 +1,6 @@
 const connect_db = require('../db/connection');
 const mongoose = require('mongoose');
 
-const StudentSchema = new mongoose.Schema({
-  name: String,
-  lastName: String,
-  email: String,
-  phone: Number,
-  course: String
-})
-const StudentModel = mongoose.model("students", StudentSchema)
 
 app.get("/students", (req, res) => {
   StudentModel.find({}).then(function(students) {
