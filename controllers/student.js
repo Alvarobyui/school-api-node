3462bed9c5f976203a05d27bd8128b3611eae307
Alvarobyui <u701860138@byui.edu>
2024-02-18 01:11:08 -0500

    validations and handling done

--- controllers/student.js
+++ controllers/student.js
@@ -49,16 +49,39 @@ const createStudent = async (req, res) => {
 
 const updateStudent = async (req, res) => {
   try {
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) {
+    const userId = req.params.id;
+    if (!ObjectId.isValid(userId)) {
       return res.status(400).json({ error: 'Invalid student ID' });
     }
+
     const { name, lastName, email, phone, course } = req.body;
     if (!name || !lastName || !email || !phone || !course) {
       return res.status(400).json({ error: 'All fields are required' });
     }
-    const student = new Student({ name, lastName, email, phone, course });
-    const response = await mongodb.getDb().db().collection('students').replaceOne({ _id: new ObjectId(id) }, student);
+
+    const existingStudent = await mongodb
+      .getDb()
+      .db()
+      .collection('students')
+      .findOne({ _id: new ObjectId(userId) });
+
+    if (!existingStudent) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    // Update existing student object with new values
+    existingStudent.name = name;
+    existingStudent.lastName = lastName;
+    existingStudent.email = email;
+    existingStudent.phone = phone;
+    existingStudent.course = course;
+
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('students')
+      .replaceOne({ _id: new ObjectId(userId) }, existingStudent);
+
     if (response.modifiedCount > 0) {
       res.status(204).json(response);
     } else {
@@ -77,7 +100,7 @@ const deleteStudent = async (req, res) => {
     }
     const response = await mongodb.getDb().db().collection('students').deleteOne({ _id: new ObjectId(id) });
     if (response.deletedCount > 0) {
-      res.status(204).send();
+      res.status(200).send();
     } else {
       res.status(404).json({ error: 'Student not found' });
     }
